3072cc98b85bbddacfc6c2e0d57329ac8ef3d422
Abhishek <abhishekparmar162@gmail.com>
2023-03-19 18:05:21 +0530

    added files

---
+++ public/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const anchor = document.querySelector('a');
+console.log(anchor.href);
+// const form = document.querySelector('form')!
+const form = document.querySelector('.new-item-form');
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+const anchor = document.querySelector('a')!
+
+console.log(anchor.href)
+
+// const form = document.querySelector('form')!
+const form = document.querySelector('.new-item-form') as HTMLFormElement
