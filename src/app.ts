c435fdd270efaa65ad33ea85f1c7d4b45f132366
Abhishek <abhishekparmar162@gmail.com>
2023-03-19 18:10:14 +0530

    added input element

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,11 @@ console.log(anchor.href)
 
 // const form = document.querySelector('form')!
 const form = document.querySelector('.new-item-form') as HTMLFormElement
+console.log(form.children)
+
+//Input
+
+const type = document.querySelector('#type') as HTMLSelectElement
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('amount') as HTMLInputElement;
