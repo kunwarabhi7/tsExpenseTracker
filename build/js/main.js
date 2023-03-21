ce67d4cc7c3628d65a03d762808c8baf82c990e9
Abhishek <abhishekparmar162@gmail.com>
2023-03-21 13:46:36 +0530

    type casting

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,1 @@
 "use strict";
-let username = 'Abhishekk';
-console.log(username);
-let a = 12;
-let b = '6';
-let c = true;
-console.log(a / b);
-console.log(a / c);
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-let username = 'Abhishekk'
-console.log(username)
-
-let a = 12
-let b = '6'
-let c = true
-
-console.log(a/b)
-console.log(a/c)
